9c7ca9bce19ce65e05384d0f03ed4435bc2cb1d1
ThesanduJ <thesandu1602500@gmail.com>
2024-10-17 20:01:39 +0530

    add order Mapping

--- src/main/java/org/example/assignment02/util/Mapping.java
+++ src/main/java/org/example/assignment02/util/Mapping.java
@@ -2,8 +2,10 @@ package org.example.assignment02.util;
 
 import org.example.assignment02.dto.impl.CustomerDTO;
 import org.example.assignment02.dto.impl.ItemDTO;
+import org.example.assignment02.dto.impl.OrderDTO;
 import org.example.assignment02.entity.impl.CustomerEntity;
 import org.example.assignment02.entity.impl.ItemEntity;
+import org.example.assignment02.entity.impl.OrderEntity;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +45,22 @@ public class Mapping {
 
     public List<ItemDTO> asItemDTOList(List<ItemEntity> itemEntityList) {
         return modelMapper.map(itemEntityList, new TypeToken<List<ItemDTO>>() {
+
+        }.getType());
+    }
+
+    //for orders
+
+    public OrderDTO toOrderDTO(OrderEntity orderEntity) {
+        return modelMapper.map(orderEntity, OrderDTO.class);
+    }
+
+    public OrderEntity toOrderEntity(OrderDTO orderDTO) {
+        return modelMapper.map(orderDTO, OrderEntity.class);
+    }
+
+    public List<OrderDTO> asOrderDTOList(List<OrderEntity> orderEntityList) {
+        return modelMapper.map(orderEntityList, new TypeToken<List<OrderDTO>>() {
 
         }.getType());
     }
