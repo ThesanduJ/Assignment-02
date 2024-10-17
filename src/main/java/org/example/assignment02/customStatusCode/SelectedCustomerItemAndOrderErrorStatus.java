265fb02f3bbb633793f1db9522de96db93c21947
ThesanduJ <thesandu1602500@gmail.com>
2024-10-17 20:03:08 +0530

    implement the OrderStatus

--- src/main/java/org/example/assignment02/customStatusCode/SelectedCustomerItemAndOrderErrorStatus.java
+++ src/main/java/org/example/assignment02/customStatusCode/SelectedCustomerItemAndOrderErrorStatus.java
@@ -5,11 +5,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.example.assignment02.dto.CustomerStatus;
 import org.example.assignment02.dto.ItemStatus;
+import org.example.assignment02.dto.OrderStatus;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class SelectedCustomerItemAndOrderErrorStatus implements CustomerStatus, ItemStatus {
+public class SelectedCustomerItemAndOrderErrorStatus implements CustomerStatus, ItemStatus, OrderStatus {
     private int statusCode;
     private String statusMessage;
 }
